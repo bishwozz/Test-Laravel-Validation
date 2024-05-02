cb482fa22250f829f1a1d32bf3e48d676c5efa23
bishwozz <bishwo123sapkota@gmail.com>
2024-05-02 12:49:19 +0545

    done

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class ArticleController extends Controller
 {
     public function store(Request $request)
-    {
+    { 
         $request->validate([
             // TASK: create your own validation rule called Uppercase
             // It should check whether title's first letter is uppercase
